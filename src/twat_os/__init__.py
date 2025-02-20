7880717f752960c6363e8f5f1599dd76407a20ec
Adam Twardoch <adam@twardoch.com>
2025-02-20 01:45:44 +0100

    Initial commit

---
+++ src/twat_os/__init__.py
@@ -0,0 +1,5 @@
+"""twat os plugin"""
+
+from importlib import metadata
+
+__version__ = metadata.version(__name__) 
---
+++ tests/test_package.py
@@ -0,0 +1,6 @@
+"""Test suite for twat_os."""
+
+def test_version():
+    """Verify package exposes version."""
+    import twat_os
+    assert twat_os.__version__
